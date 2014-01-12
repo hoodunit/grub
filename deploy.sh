8da6438b79874064c189e78b881bf60c477b05e2
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-01-12 18:59:46 +0200

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git branch -D deploy
+git checkout -b deploy
+lein cljsbuild once prod
+git add --force public/js/grub.js
+git commit -m "Add client code for Heroku deployment"
+git push --force heroku deploy:master
+git checkout master
