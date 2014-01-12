091aa3c1d765f6b383727ed21983750b9898a614
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-01-12 14:59:43 +0200

    Use core.async from Maven

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1909"]
-                 [core.async "0.1.0-SNAPSHOT"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [http-kit "2.1.8"]
                  [compojure "1.1.5"]
                  [ring/ring-devel "1.2.0"]
