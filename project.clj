24a077b2770b5361b341218c8878b59c0fd023f1
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2014-10-10 18:00:13 +0300

    Update Clojure, ClojureScript, Om, and core.async

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2277"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [om "0.7.3"]
                  [http-kit "2.1.18"]
                  [compojure "1.1.8"]
                  [ring/ring-core "1.3.0" :exclusions [org.clojure/tools.reader]]
@@ -15,7 +16,6 @@
                  [com.novemberain/monger "2.0.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [clj-webdriver "0.6.1" :exclusions [org.clojure/core.cache]]
-                 [om "0.7.0"]
                  [sablono "0.2.17"]
                  [cljs-uuid "0.0.4"]
                  [net.polyc0l0r/hasch "0.2.3"]]
