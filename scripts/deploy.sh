48067a00f6f0db8ca991d5e0508a81a6cd28a66a
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-05-01 09:01:22 +0300

    Deploy script just copies files over

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 BUILD_NAME=`ls -t target/builds | head -1`
-#scp target/builds/$BUILD_NAME $GRUB_USER@$GRUB_SERVER:$GRUB_STAGING_DIR/$BUILD_NAME
-ssh $GRUB_USER@$GRUB_SERVER "cp $GRUB_STAGING_DIR/$BUILD_NAME $GRUB_RELEASE_DIR/$BUILD_NAME && killall java && run_server.sh"
+scp target/builds/$BUILD_NAME $GRUB_USER@$GRUB_SERVER:$GRUB_RELEASE_DIR/$BUILD_NAME
