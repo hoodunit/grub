6794e81ea91788a44adc5e7db054c10cfd019925
Nicholas Kariniemi <nicholas.kariniemi@gmail.com>
2015-06-16 19:55:04 +0300

    Update from clojure 1.7.0-alpha1 -> 1.7.0-RC1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha1"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.7.3"]
